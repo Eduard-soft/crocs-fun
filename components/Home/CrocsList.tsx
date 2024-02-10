a20fe8bd12723211d91895f27d30c0b94ff16352
Eduard-soft <92379627+Eduard-soft@users.noreply.github.com>
2024-02-10 12:12:57 +0300

    Card editing
    Signed-off-by: Eduard-soft <92379627+Eduard-soft@users.noreply.github.com>

--- components/Home/CrocsList.tsx
+++ components/Home/CrocsList.tsx
@@ -3,7 +3,7 @@ import CrocsCard from './CrocsCard';
 
 function CrocsList(props:any) {
 	return (
-		<div className='grid grid-cols-3 first-letter:md:grid-cols-2'>
+		<div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
        {props.crocsList.map((crocs: any, index: number) => (
 				<div>
 					<CrocsCard crocs = {crocs} />
